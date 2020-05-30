e48935a43b3f801f2ff50d364018c3be8e6ed092
kunstix <scroovy@web.de>
2020-05-30 11:46:09 +0200

    buttons for colorbox

--- src/components/colors/ColorBox.jsx
+++ src/components/colors/ColorBox.jsx
@@ -3,10 +3,17 @@ import './ColorBox.css';
 
 export default class ColorBox extends Component {
   render() {
+    const { name, bg } = this.props;
+
     return (
-      <div style={{ backgroundColor: this.props.bg }} className='ColorBox'>
-        <span>{this.props.name}</span>
-        <span>MORE</span>
+      <div style={{ backgroundColor: bg }} className='ColorBox'>
+        <div className='copy-container'>
+          <div className='box-content'>
+            <span>{name}</span>
+          </div>
+          <button className='copy-button'>Copy</button>
+        </div>
+        <span className='more'>MORE</span>
       </div>
     );
   }
