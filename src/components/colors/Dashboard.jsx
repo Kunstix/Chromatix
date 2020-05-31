c1311c89560e9a326f84536dbeacbff2d7d446d3
kunstix <scroovy@web.de>
2020-05-31 12:07:10 +0200

    PalettePreview

--- src/components/colors/Dashboard.jsx
+++ src/components/colors/Dashboard.jsx
@@ -7,13 +7,19 @@ class Dashboard extends Component {
   render() {
     const { palettes } = this.props;
     return (
-      <div>
-        <h1>Chromatix</h1>
-        {palettes.map((palette, index) => (
-          <Link key={index} to={`/palette/${palette.id}`}>
-            <PalettePreview {...palette} />
-          </Link>
-        ))}
+      <div className='Dashboard'>
+        <div className='palettes-container'>
+          <nav className='palettes-nav'>
+            <h5>Chromatix</h5>
+          </nav>
+          <div className='palettes-board'>
+            {palettes.map((palette, index) => (
+              <Link key={index} to={`/palette/${palette.id}`}>
+                <PalettePreview {...palette} />
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
